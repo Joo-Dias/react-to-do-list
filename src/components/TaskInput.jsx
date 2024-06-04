7708674bf36c421b152f47aa39c2e9327aa0e8b5
FATEC | João Victor Soares Dias <fatec.jdias@icolabora.com.br>
2024-06-04 18:33:22 -0300

    estruturando e estilizando os componentes

---
+++ src/components/TaskInput.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TaskInput = () => {
+  return (
+    <>
+        <form>
+            <input type="text" placeholder='Adicione uma nova tarefa...'/>
+            <button type='submit'>Adicionar</button>
+        </form>
+    </>
+  )
+}
+
+export default TaskInput
